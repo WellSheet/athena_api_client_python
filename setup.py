ce62ed509654f0d2856a2eb5b4df3a5931ac0cbb
Ryan Lee <ryan.lee@wellsheet.com>
2017-12-13 12:23:54 -0800

    Add distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='AthenaClient',
+    version='0.0.1',
+    author='Ryan Lee',
+    author_email='ryan.lee@wellsheet.com',
+    packages=['athena'],
+    description='Athena API Python client',
+    long_description=open('README.md').read(),
+)
